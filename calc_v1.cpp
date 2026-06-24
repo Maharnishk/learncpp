48e0d6acf85be15f89122658fd9bab8ca5d9c2c8
Maharnishk <iammaharnish@gmail.com>
2026-06-24 15:55:56 +0530

    simple calculator

---
+++ calc_v1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+void addition(int a, int b){
+   int result = a+b;
+  std::cout << result << std :: endl;
+}
+void sub (int a, int b){
+   int result = a-b;
+   std::cout << result << std :: endl;
+}
+void  mul(int a, int b){
+   int result = a*b;
+   std::cout << result << std :: endl;
+}
+void division(int a, int b){
+   int result = a/b;
+   std::cout << result << std :: endl;
+}
+int main(){
+    int option;
+    int num1 , num2;
+    std::cout << "what calculation u wanna perform?"<< std :: endl;
+    std::cout << "option 1 = Addition"<< std :: endl;
+    std::cout << "option 2 = substraction"<< std :: endl;
+    std::cout << "option 3 = multiplication"<< std :: endl;
+    std::cout << "option 4 = division"<< std :: endl;
+    std::cout << "ur option ->";
+    std :: cin>>option;
+    std::cout << "Enter ur number 1 : "<< std :: endl;
+    std :: cin >> num1;
+    std::cout << "Enter ur number 2 : "<< std :: endl;
+    std :: cin >> num2;
+
+    if (option == 1){
+    addition(num1,num2);
+    }
+    else if(option==2)
+    {
+        sub(num1,num2);
+    }
+    else if(option==3)
+    {
+        mul(num1,num2);
+    }
+    else if(option==4)
+    {
+        division(num1,num2);
+    }
+    else 
+    {
+        std :: cout << "operator not found try 1,2,3,4 : "<< std :: endl;
+    }
+   
+    }
+
