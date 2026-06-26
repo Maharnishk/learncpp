82e22f8a9c3971c5ece44aab7284f8f4806ee94e
Maharnishk <iammaharnish@gmail.com>
2026-06-26 23:54:24 +0530

    Linker_03

---
+++ log.h
@@ -0,0 +1,12 @@
+
+#pragma once 
+static void Log(const char* message);
+/*
+instaed of static we can also use inline
+inline void Log(const char* message){
+    std :: cout << message << std :: endl;
+}
+ inline just make that Log("Initailize Log ") to
+  std :: cout <<"Initailize Log "<< std :: endl; 
+
+*/
