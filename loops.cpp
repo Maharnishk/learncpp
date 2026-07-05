e9b8529cd2c9e6d1fff0098036b6dd12a69ae9f8
Maharnishk <iammaharnish@gmail.com>
2026-07-06 01:31:07 +0530

    for/while/do-while--loopsss

---
+++ loops.cpp
@@ -0,0 +1,26 @@
+//loops
+#include <iostream>
+
+int main(){
+   //for-loop 
+    for(int i=1;i<=8;i++){
+       std :: cout << "hello world " << std :: endl;
+    }
+
+    std :: cout <<"==========================="<<std :: endl;
+    //while-loop
+    int i = 0;
+    while(i <5){
+   std:: cout << "hello 2nd world  "<< std:: endl;
+      i++;
+    }
+    std :: cout <<"==========================="<<std :: endl;
+
+
+    //do-while loop
+    int io =23;
+   do{
+      std :: cout << "do while executed"<< std :: endl;
+      io++;
+   } while(i>40);   
+}
